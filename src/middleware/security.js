9263e38136d02cd141997b0868ea44634887ccff
vnitincse2024-spec <vnitin.cse2024@citchennai.net>
2025-11-15 18:59:33 +0530

    Create security.js

---
+++ src/middleware/security.js
@@ -0,0 +1,9 @@
+// Custom security headers middleware
+export const securityHeaders = (req, res, next) => {
+  res.set({
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'DENY',
+    'Strict-Transport-Security': 'max-age=31536000; includeSubDomains'
+  });
+  next();
+};
